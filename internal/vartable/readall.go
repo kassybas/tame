941243061825f17d7cd34104f954572964e6e5e4
Barnabas Kutassy <kutassy.b@gmail.com>
2020-01-28 11:37:43 +0100

    WIP

--- internal/vartable/readall.go
+++ internal/vartable/readall.go
@@ -4,10 +4,10 @@ func (vt *VarTable) GetAllEnvVars(ShellFieldSeparator string) []string {
 	formattedVars := []string{}
 	vt.RLock()
 	allVars := vt.vars
-	vt.RUnlock()
 	for _, v := range allVars {
 		formattedVars = append(formattedVars, v.ToEnvVars(ShellFieldSeparator)...)
 	}
+	vt.RUnlock()
 	return formattedVars
 }
 
@@ -15,9 +15,9 @@ func (vt *VarTable) GetAllValues() map[string]interface{} {
 	vars := make(map[string]interface{})
 	vt.RLock()
 	allVars := vt.vars
-	vt.RUnlock()
 	for k, v := range allVars {
 		vars[k] = v.Value()
 	}
+	vt.RUnlock()
 	return vars
 }
